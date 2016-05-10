5bade01bb2f62ecf63d0804e2e8dd34ec9950ec3
Tiffany Yu <tiffanyayu@gmail.com>
2016-05-10 17:57:52 +0200

    Added body parser

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 var express = require ('express');
+var bodyParser = require ('body-parser');
 var fs = require ('fs');
 
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set ('views', './src/views');
 app.set ('view engine', 'jade');
@@ -19,6 +22,21 @@ app.get ('/', function (request, response) {
 	});
 });
 
+// PART 1
+app.get ('/form', function (request, response) {
+	response.render ('form');
+});
+
+app.post ('/form', function (request, response) {
+	console.log ( 'search request received' );
+	console.log ( request.body );
+
+	response.send ( 'data received: ' + JSON.stringify(request.body) + '\n')
+});
+
 var server = app.listen(3000, function() {
 	console.log('Example app listening on port: ' + server.address().port);
 });
+
+
+
