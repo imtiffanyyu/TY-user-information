56fb0d6595274ba6a9e3171bb6be01407c8ba06b
Tiffany Yu <tiffanyayu@gmail.com>
2016-05-12 14:54:17 +0200

    Route 3 of the user information assignment

--- src/app.js
+++ src/app.js
@@ -4,12 +4,13 @@ var fs = require ('fs');
 
 var app = express();
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set ('views', './src/views');
 app.set ('view engine', 'jade');
 
-// PART 0
+// ROUTE 1
 app.get ('/', function (request, response) {
 	fs.readFile('./resources/users.json', function (error, data) {
 		if (error) {
@@ -22,7 +23,7 @@ app.get ('/', function (request, response) {
 	});
 });
 
-// PART 1
+// ROUTE 2
 app.get ('/form', function (request, response) {
 	response.render ('form');
 });
@@ -31,8 +32,32 @@ app.post ('/form', function (request, response) {
 	console.log ( 'search request received' );
 	console.log ( request.body );
 
-	response.send ( 'data received: ' + JSON.stringify(request.body) + '\n')
+	fs.readFile('./resources/users.json', function (error, data) {
+		var matchingUser = [];
+		if (error) {
+			console.log (error);
+		}
+
+		var parsedData = JSON.parse(data);
+		
+		for ( var i = 0; i < parsedData.length; i++ ) {
+			if ( request.body.name == parsedData[i].firstname || request.body.name == parsedData[i].lastname ) {
+				matchingUser.push(parsedData[i]);
+			}
+		}
+		
+		console.log (matchingUser);
+		response.render ('results', { user: JSON.stringify(matchingUser) });
+		
+	});
+	
 });
+
+// ROUTE 3. not quite sure this works yet
+app.get ('/results', function (request, response) {
+	response.render ('results');
+});
+
 
 var server = app.listen(3000, function() {
 	console.log('Example app listening on port: ' + server.address().port);
