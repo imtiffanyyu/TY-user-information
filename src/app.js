01d5875042f95da209e5fdea98ff85879792066b
Tiffany Yu <tiffanyayu@gmail.com>
2016-05-12 17:06:20 +0200

    Allows you to add new users

--- src/app.js
+++ src/app.js
@@ -44,18 +44,48 @@ app.post ('/form', function (request, response) {
 			if ( request.body.name == parsedData[i].firstname || request.body.name == parsedData[i].lastname ) {
 				matchingUser.push(parsedData[i]);
 			}
+			else {
+				matchingUser = request.body.name + ' not found';
+			}
 		}
 		
 		console.log (matchingUser);
 		response.render ('results', { user: JSON.stringify(matchingUser) });
 		
 	});
-	
+
 });
 
-// ROUTE 3. not quite sure this works yet
-app.get ('/results', function (request, response) {
-	response.render ('results');
+// // ROUTE 3. not quite sure this works yet
+// app.get ('/results', function (request, response) {
+// 	response.render ('results');
+// });
+
+// ROUTE 4 + 5
+app.get ('/addnew', function (request, response) {
+	response.render ('addnew');
+});
+
+app.post ('/addnew', function (request, response) {
+	console.log ( 'add request received' );
+	console.log ( request.body );
+
+	fs.readFile('./resources/users.json', function (error, data) {
+		var newUser = request.body;
+		if (error) {
+			console.log (error);
+		}
+
+		var parsedData = JSON.parse(data);
+		
+		parsedData.push(newUser);
+		console.log (parsedData);
+		
+		fs.writeFile ('./resources/users.json', JSON.stringify(parsedData, null, 4), function (error) {
+			if (error) throw error;
+		});
+	});
+	response.redirect ('/');
 });
 
 
