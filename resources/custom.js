90b3ec16707c627e04ba4acebe2dba76dcb74cb1
Tiffany Yu <tiffanyayu@gmail.com>
2016-05-18 18:07:38 +0200

    Working on the time part

--- resources/custom.js
+++ resources/custom.js
@@ -10,17 +10,41 @@ $ ( document ).ready ( function ( ) {
 		console.log (ajaxdata)
 
 
-		if (ajaxdata.name) {
-				$.post('/results', ajaxdata, function ( data ) {
-					
-					var empty = $('#results').empty();
+// //
+// // Throttle calls to "callback" routine and ensure that it
+// // is not invoked any more often than "delay" milliseconds.
+// //
+// function throttle(delay, callback) {
+//     var previousCall = new Date().getTime();
+//     return function() {
+//         var time = new Date().getTime();
 
-					for (var person in data) {
-						$ (empty).append('<option>'+ data[person].firstname + ' ' + '<option>' + data[person].lastname)
-					}
-					console.log(data)
-				})
-		}
+//         //
+//         // if "delay" milliseconds have expired since
+//         // the previous call then propagate this call to
+//         // "callback"
+//         //
+//         if ((time - previousCall) >= delay) {
+//             previousCall = time;
+//             callback.apply(null, arguments);
+//         }
+//     };
+// }
+
+
+		// $(window).scroll(throttle(30, function() {
+			if (ajaxdata.name) {
+					$.post('/results', ajaxdata, function ( data ) {
+						
+						var empty = $('#results').empty();
+
+						for (var person in data) {
+							$ (empty).append('<option>'+ data[person].firstname + ' ' + '<option>' + data[person].lastname)
+						}
+						console.log(data)
+					})
+			}
+		// }));
 
 	})
 })
